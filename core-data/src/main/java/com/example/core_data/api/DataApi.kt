fee107812460e0e7517d4037c6d26f1672064d1b
Andrey Bordukov <andrbor02@yandex.ru>
2023-01-22 00:05:38 +0300

    refactor: update imports in data api and dependencies

--- core-data/src/main/java/com/example/core_data/api/DataApi.kt
+++ core-data/src/main/java/com/example/core_data/api/DataApi.kt
@@ -1,6 +1,8 @@
 package com.example.core_data.api
 
-import com.example.core_data.impl.di.BaseApi
+import com.example.core_data.api.repository.SettingsRepository
+import com.example.core_data.api.repository.TaskRepository
+import com.example.core_model.BaseApi
 
 interface DataApi: BaseApi {
 
