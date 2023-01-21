3d36611c81aa3cefb7a5c149508fe513d6d9ecd7
Andrey Bordukov <andrbor02@yandex.ru>
2023-01-21 23:06:57 +0300

    feat: move and refactor SettingsDataSource.kt

--- core-database/src/main/java/com/example/core_database/impl/datasource/sharedprefs/SharedPreferencesSettingsDataSource.kt
+++ core-database/src/main/java/com/example/core_database/impl/datasource/sharedprefs/SharedPreferencesSettingsDataSource.kt
@@ -2,24 +2,25 @@ package com.example.core_data.impl.datasource.sharedprefs
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.example.core_database.impl.datasource.SettingsDataSource
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class SharedPreferencesSettingsDataSource @Inject constructor(
     context: Context
-) {
+) : SettingsDataSource {
     private val showCompletedKey = "SHOW_COMPLETED_FLAG"
     private val settingsPreference: SharedPreferences =
         context.getSharedPreferences(showCompletedKey, Context.MODE_PRIVATE)
 
-    fun setShowCompletedFlag(isShown: Boolean) {
+    override fun setShowCompletedFlag(isShown: Boolean) {
         val editor = settingsPreference.edit()
         editor.putBoolean(showCompletedKey, isShown)
         editor.apply()
     }
 
     // TODO: Handle if value doesn't exist
-    fun getShowCompletedFlag() = flowOf(
+    override fun getShowCompletedFlag() = flowOf(
         settingsPreference.getBoolean(showCompletedKey, true)
     )
 }
