32c88aecbbc9973d72aaf855c5931c16d565d44c
Andrey Bordukov <andrbor02@yandex.ru>
2023-01-24 02:39:48 +0300

    feat: inject network client into repository

--- core-data/src/main/java/com/example/core_data/impl/repository/TaskRepositoryImpl.kt
+++ core-data/src/main/java/com/example/core_data/impl/repository/TaskRepositoryImpl.kt
@@ -5,27 +5,38 @@ import androidx.annotation.WorkerThread
 import com.example.core_data.api.repository.TaskRepository
 import com.example.core_data.impl.mapper.DataToDomainTaskMapper
 import com.example.core_data.impl.mapper.DomainToDataTaskMapper
+import com.example.core_data.impl.mapper.DomainToNetworkWithIdTaskMapper
 import com.example.core_data.impl.model.DatabaseTask
 import com.example.core_database.impl.client.DatabaseClient
 import com.example.core_model.Task
+import com.example.core_network.impl.client.NetworkClient
 import com.example.core_utils.datawrappers.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 class TaskRepositoryImpl @Inject constructor(
     private val domainToDataTaskMapper: DomainToDataTaskMapper,
     private val dataToDomainTaskMapper: DataToDomainTaskMapper,
+    private val domainToNetworkWithIdTaskMapper: DomainToNetworkWithIdTaskMapper,
     databaseClient: DatabaseClient,
+    networkClient: NetworkClient,
 ) : TaskRepository {
 
     private val taskDataSource = databaseClient.getTaskDataSource()
-
+    private val networkDataSource = networkClient.getTaskRemoteDataSource()
 
     private val dbTaskList: Flow<List<DatabaseTask>> = taskDataSource.getTaskList()
 
     private val taskList: Flow<List<Task>> = dbTaskList.map { dbList ->
         dbList.map { databaseTask ->
             dataToDomainTaskMapper(databaseTask)
+        }
+    }
+
+    private var networkList = dbTaskList.map {
+        it.map {
+            domainToNetworkWithIdTaskMapper(dataToDomainTaskMapper(it))
         }
     }
 
