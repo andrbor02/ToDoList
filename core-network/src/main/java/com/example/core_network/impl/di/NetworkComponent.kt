f0cb07b636b3fbbf472df1b1780bd2544d78c7f3
Andrey Bordukov <andrbor02@yandex.ru>
2023-01-24 02:32:07 +0300

    feat: configure di for network

--- core-network/src/main/java/com/example/core_network/impl/di/NetworkComponent.kt
+++ core-network/src/main/java/com/example/core_network/impl/di/NetworkComponent.kt
@@ -1,20 +1,35 @@
 package com.example.core_network.impl.di
 
-//@Singleton
-//@Component(
-//    dependencies = [NetworkDependencies::class],
-//    modules = [NetworkModule::class]
-//)
-//interface NetworkComponent : NetworkApi {
-//
-//    @Component.Factory
-//    interface Factory {
-//        fun create(dependencies: NetworkDependencies): NetworkComponent
-//    }
-//
-//    companion object {
-//        fun initAndGet(dependencies: NetworkDependencies): NetworkComponent {
-//            return DaggerNetworkComponent.factory().create(dependencies)
-//        }
-//    }
-//}
+import com.example.core_network.api.NetworkApi
+import com.example.core_network.api.NetworkDependencies
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    dependencies = [NetworkDependencies::class],
+    modules = [NetworkModule::class]
+)
+interface NetworkComponent : NetworkApi {
+
+    @Component.Factory
+    interface Factory {
+        fun create(dependencies: NetworkDependencies): NetworkComponent
+    }
+
+    companion object {
+
+        private var component: NetworkComponent? = null
+
+        fun initAndGet(dependencies: NetworkDependencies): NetworkComponent {
+            if (component == null) {
+                synchronized(NetworkComponent::class.java) {
+                    if (component == null) {
+                        component = DaggerNetworkComponent.factory().create(dependencies)
+                    }
+                }
+            }
+            return component!!
+        }
+    }
+}
